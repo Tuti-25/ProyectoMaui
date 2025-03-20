77b0dff3b2c45627718aad9d4f4027813aee5b5d
Tuti-25 <tutidecalle076@gmail.com>
2025-03-19 22:09:59 -0600

    Agregando la vista de login

--- AppMaui/AppMaui/Paginas/LoginOrSignUp.xaml.cs
+++ AppMaui/AppMaui/Paginas/LoginOrSignUp.xaml.cs
@@ -6,4 +6,9 @@ public partial class LoginOrSignUp : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void BtnLogIn_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Login());
+    }
 }
