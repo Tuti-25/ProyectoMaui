3a216a0ed518291a5c2f45276c1f20ddaeb4bf9c
Valeria <vale67690@gmail.com>
2025-04-05 00:39:49 -0600

    *Menu*

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MenuView.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent(); // Solo esta línea
+        }
+    }
+}
